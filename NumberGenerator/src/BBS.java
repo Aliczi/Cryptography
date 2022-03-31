92fedb0fdaf44a15aacfe086510dd414964fe3ac
Alicja <alicja.miloszewska@student.put.poznan.pl>
2022-03-31 11:48:40 +0200

    Add Blum Blum Shub pseudorandom number generator

---
+++ NumberGenerator/src/BBS.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+public class BBS {
+
+    public static void numberGenerator(long blum_num, long random_a, long bits_num, File outputFile) throws IOException {
+
+        FileOutputStream outputStream = new FileOutputStream(outputFile);
+
+        long x0,xi;
+        x0 = (random_a*random_a)%blum_num;
+
+        for(int i=1; i<bits_num; i++) {
+
+            xi=(x0*x0)%blum_num;
+
+            if(((int)xi&1) == 1) outputStream.write('1');
+            else outputStream.write('0');
+
+            x0=xi;
+        }
+        outputStream.close();
+    }
+
+
+}
+
---
+++ NumberGenerator/src/Main.java
@@ -0,0 +1,35 @@
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+
+        public static void main(String[] args) throws IOException {
+
+            long p, q, n,a;
+
+        /*
+        7 11 19 23 31 43 47 59 67 71 79 83 103 107 131 139 151 163 167 179
+        191 199 211 223 227 239 251 263 271 283 307 311 331 347 359 367 379 383 419 431
+        1707892411793 2290939653701
+        */
+
+            // 1stprime number p
+            p = 311L;
+            // 2nd prime number q
+            q = 431L;
+            //Blum's number
+            n = p*q;
+            //a -> NWD(a, n) = 1. Between 2 and n-1
+            a=157L;
+
+
+            long bits_num=20000;
+            String file_name="src/seq_"+bits_num+"_"+n+"_"+a+".txt";
+
+            File outputFile = new File(file_name);
+            BBS.numberGenerator(n, a,bits_num, outputFile );
+
+
+        }
+}
+
