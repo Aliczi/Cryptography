88679bb415bee5c65c1766a85f44f7a0eb7a54ab
Alicja <alicja.miloszewska@student.put.poznan.pl>
2022-04-03 12:53:32 +0200

    Bug fix - first byte was not written to the output file

--- NumberGenerator/src/BBS.java
+++ NumberGenerator/src/BBS.java
@@ -12,6 +12,9 @@ public class BBS {
         long x0,xi;
         x0 = (random_a*random_a)%blum_num;
 
+        if(((int)x0&1) == 1) outputStream.write('1');
+        else outputStream.write('0');
+
         for(int i=1; i<bits_num; i++) {
 
             xi=(x0*x0)%blum_num;
